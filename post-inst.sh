d631769d400b3ff1f631636a9cc0e6289bfbc999
Guillaume Espanel <guillaume.espanel@objectif-libre.com>
2016-02-29 23:14:31 +0100

    Fix typo in post inst

--- post-inst.sh
+++ post-inst.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-useradd -r gitlab-webhook-receiver -h /var/lib/gitlab-webhook-receiver -m
+useradd -r gitlab-webhook-receiver -d /var/lib/gitlab-webhook-receiver -m
 mkdir -p /var/log/gitlab-webhook-receiver
 chown gitlab-webhook-receiver: /var/log/gitlab-webhook-receiver
