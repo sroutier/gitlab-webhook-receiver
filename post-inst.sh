333aca738035a9b08fd627cc99d69d0e32d5183c
Guillaume Espanel <guillaume.espanel@objectif-libre.com>
2016-02-29 23:12:41 +0100

    Launch as a non-priviledged user

--- post-inst.sh
+++ post-inst.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-useradd -r gitlab-webhook-receiver
+useradd -r gitlab-webhook-receiver -h /var/lib/gitlab-webhook-receiver -m
+mkdir -p /var/log/gitlab-webhook-receiver
+chown gitlab-webhook-receiver: /var/log/gitlab-webhook-receiver
--- post-uninst.sh
+++ post-uninst.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
+test -d /var/log/gitlab-webhook-receiver && chown -R nobody: /var/log/gitlab-webhook-receiver
 userdel gitlab-webhook-receiver
