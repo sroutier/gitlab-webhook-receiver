17997e894f1900a1395536071169a135739a7ab8
Guillaume Espanel <guillaume.espanel@objectif-libre.com>
2016-02-29 05:53:12 +0100

    Package it

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ config = {
     'from git lab and sends them to Puppet',
     'author': 'Shawn Sterling Guillaume Espanel',
     'url': 'https://github.com/ObjectifLibre/gitlab-webhook-receiver',
-    'author_email': 'shawn@systemtemplar.org, guillaume.espanel@objectif-libre.com',
+    'author_email': 'shawn@systemtemplar.org, '
+                    'guillaume.espanel@objectif-libre.com',
     'version': '1.0',
     'install_requires': ['requests'],
     'packages': ['gitlab_webhook_receiver'],
     'entry_points': {'console_scripts':
-                     ['gitlab-webhook-receiver = gitlab_webhook_receiver:main']},
-    'name': 'gitlab-webhook-receiver'
+                     ['gitlab-webhook-receiver=gitlab_webhook_receiver.gitlab_webhook_receiver:main']
+                     },
+    'name': 'gitlab-webhook-receiver',
+    'data_files': [('/etc', ['etc/gitlab-webhook-receiver.conf']),
+                   ('/etc/init', ['etc/init/gitlab-webhook-receiver.conf']),
+                   ]
 }
 
 setup(**config)
